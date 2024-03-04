073089d2c6dd9fa9c2809e9b92f68b0262047051
Rahemin <42874260+Rahemin@users.noreply.github.com>
2024-03-04 16:47:11 +0600

    Add files via upload

---
+++ lab5.2/forgotpassword.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>FORM</title>
+</head>
+<body>
+        <form method="" action="" enctype="">
+            <fieldset>
+                <legend>FORGOT PASSWORD</legend>
+  
+                Email:      <input type="email" name="email" value="" /><br>
+                
+
+               
+                            <input type="submit" name="submit" value="Submit" />
+                
+
+
+            </fieldset>
+        </form>
+</body>
+</html>
